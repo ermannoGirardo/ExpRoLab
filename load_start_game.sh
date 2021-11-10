972ef6ad1319bbc0f4b5ea239f9db49baf5379b6
ermannoGirardo <48509825+ermannoGirardo@users.noreply.github.com>
2021-11-10 01:32:38 +0100

    Version 2.1

--- load_start_game.sh
+++ load_start_game.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gnome-terminal --tab --title="start_nodes_load_ontology" -- bash -c "roslaunch exproblab_assignment_1 start_load.launch"
-gnome-terminal --tab --title="cluedo_fsm" -- bash -c "sleep 3; rosrun exproblab_assignment_1 cluedo_fsm.py "
+gnome-terminal --tab --title="cluedo_fsm" -- bash -c "sleep 5; rosrun exproblab_assignment_1 cluedo_fsm.py "
